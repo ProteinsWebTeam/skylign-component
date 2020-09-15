df7644648bec872049b9d1b16b1f55b498375734
CodeGit <maq@ebi.ac.uk>
2020-09-15 14:21:07 +0100

    found a jest dom with custom component support

--- src/_tests_/test.js
+++ src/_tests_/test.js
@@ -1,9 +1,13 @@
 import Skylign from "../index.js";
-//import data from './PF00001_logo.json';
+import pf1 from './PF00001_logo.json';
+import pf2 from './PF00002_logo.json';
 
 test("Instantiate skylign", () => {
   const skylign = new Skylign();
   expect(skylign).toBeInstanceOf(Skylign);
-  const data = require('./PF00001_logo.json');
-  console.log(`DATA: ${data}`);
 });
+
+// test("Set logo attribute", () => {
+//   const skylign = document.createElement('skylign-component');
+//
+// });
--- src/index.js
+++ src/index.js
@@ -13,12 +13,4 @@ if (window.customElements) {
     loadComponent();
   });
 }
-
-//BEGIN TODO: remove this development code
-import Data from './_tests_/PF00001_logo.json';
-const skylign = document.createElement('skylign-component');
-skylign.setAttribute('logo', JSON.stringify(Data));
-document.body.appendChild(skylign);
-//END TODO
-
 export default Skylign;
